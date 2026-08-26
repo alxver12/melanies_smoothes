f94a0483cd9c67c3b2cc097f175a83c2928da0c5
alxver12 <vergara.alex128@gmail.com>
2026-08-26 11:17:25 -0500

    Update Smoothiefroot API URL for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,7 @@ if ingredients_list:
 
 
 #New section to display smoothiefroot nutrition information 
-import requests 
-smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
-
     
